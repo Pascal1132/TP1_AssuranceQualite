57420b99da927d2724c5dcf64e0b2efe2d06dc63
Pascal <pas.parent@outlook.com>
2020-02-04 11:06:47 -0500

    remise en norme le nom des variables et finalisation de la méthode affiche()

--- src/CV.java
+++ src/CV.java
@@ -1,24 +1,29 @@
 
 public class CV {
-    public String Nom = "";
-    public String Prenom = "";
-    public String Formation = "";
-    public int Experience = 0;
-    public String[] Competences = {"", ""};
-    public String Attentes = "";
+    public String nom = "";
+    public String prenom = "";
+    public String formation = "";
+    public int experience = 0;
+    public String[] competences = {"", ""};
+    public String attentes = "";
     
     public CV(String nom, String prenom, String formation, int experience, String[] competences, String attentes) {
-        Nom = nom;
-        Prenom = prenom;
-        Formation = formation;
-        Experience = experience;
-        Competences = competences;
-        Attentes = attentes;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.formation = formation;
+        this.experience = experience;
+        this.competences = competences;
+        this.attentes = attentes;
     }
     
     public void affiche() {
-        System.out.println("Nom: " + Nom + "\nPrenom: " + Prenom + "\nFormation: " + Formation
-                + "\nExpérience: " + Experience + " ans\nCompétences: " + Competences + "\nAttentes: " + Attentes);
+        String competencesString = "";
+        
+        for (String competence : competences) {
+			competencesString += competence + ";";
+		}
+		System.out.println("\nNom: " + nom + "\nPrenom: " + prenom + "\nFormation: " + formation
+                + "\nExpérience: " + experience + " ans\nCompétences: "+ competencesString + "\nAttentes: " + attentes);
             
     }
     
@@ -27,6 +32,7 @@ public class CV {
         CV bergeronCV =new CV("Cédrik","Bergeron","Éboueur 101",4,new String[] {"Création de tables de chevets", "Peinture à l'acrylique"},"Cours passionnant");
         CV parentCV =new CV("Pascal","Parent","Machiniste",1,new String[] {"Design d'application", "Musique"},"Cours qui est moins plate que celui de français de la derniere session.");
         bergeronCV.affiche();
+        
         parentCV.affiche();
         
     }
