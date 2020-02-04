51a96622e0fc14686fbc43fc28892ece79ff32f9
Pascal <pas.parent@outlook.com>
2020-02-04 10:53:10 -0500

    création constructeur et affiche() CV

--- src/CV.java
+++ src/CV.java
@@ -1,4 +1,35 @@
 
 public class CV {
+    public String Nom = "";
+    public String Prenom = "";
+    public String Formation = "";
+    public int Experience = 0;
+    public String[] Competences = {"", ""};
+    public String Attentes = "";
+    
+    public CV() {
+        
+    }
+    public CV(String nom, String prenom, String formation, int experience, String[] competences, String attentes) {
+        Nom = nom;
+        Prenom = prenom;
+        Formation = formation;
+        Experience = experience;
+        Competences = competences;
+        Attentes = attentes;
+    }
+    
+    public void affiche() {
+        System.out.println("Nom: " + Nom + "\nPrenom: " + Prenom + "\nFormation: " + Formation
+                + "\nExpérience: " + Experience + " ans\nCompétences: " + Competences + "\nAttentes: " + Attentes);
+            
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Bienvenue chez Barette!");
+        
+        
+    }
 
 }
+ 
