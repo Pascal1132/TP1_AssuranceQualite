486d7a1f27d278bc7f2ef7f5e1928b986ff01a19
CedrikBergeron <cedrik.bergeron@me.com>
2020-02-18 10:58:17 -0500

    Ajout de la classe Client

---
+++ src/partie2/Client.java
@@ -0,0 +1,17 @@
+package partie2;
+
+public class Client {
+	public String nom = "";
+	
+	public Client(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public void setNom( String nom ) {
+		this.nom = nom;
+	}
+}
